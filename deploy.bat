49f96fa89bdd7df8cfacc455611557b7b2012e1f
luna <iszychen@gmail.com>
2020-09-28 22:10:28 +0800

    'auto-deploy'

--- deploy.bat
+++ deploy.bat
@@ -11,8 +11,8 @@ git remote add origin https://gitee.com/luna_nov/blog.git
 git remote add github https://github.com/czy1024/blog.git
 
 git pull
-git push --force origin HEAD:gh-pages
-git push --force github HEAD:gh-pages
+git push  origin HEAD:gh-pages
+git push  github HEAD:gh-pages
 
 echo Auto-Deploy-Complete!
 pause
